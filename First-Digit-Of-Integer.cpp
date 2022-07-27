0a7826d0c08b4896cc449cd820a03db2b051282b
Krithik22 <94282684+Krithik22@users.noreply.github.com>
2022-07-28 00:46:13 +0530

    commit

---
+++ First-Digit-Of-Integer.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  //write your code here
+  int testCases,i,n;
+  scanf("%d",&testCases);
+  while(testCases){
+    scanf("%d",&n);
+    printf("%d\n",n/1000);
+    testCases--;
+  }  
+  return 0;
+}
