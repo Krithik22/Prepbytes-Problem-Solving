04a864e2175b2c98ff7a9aace06fb5fcec7378d4
Krithik22 <94282684+Krithik22@users.noreply.github.com>
2022-07-28 00:47:37 +0530

    commit

---
+++ Multiply-Numbers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include<cstdio>
+using namespace std;
+
+int main()
+{
+  //write your code here
+  int x,y;
+  
+  scanf("%d %d",&x,&y);
+  printf("%d",x*y);
+  
+  return 0;
+}
